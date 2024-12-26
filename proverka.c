1782cc43eff0ea4bd3723efc5c83d07125ecae86
Education-Avanti <robo@avgr.tech>
2024-12-26 16:41:02 +0300

    V0.04

---
+++ proverka.c
@@ -0,0 +1,14 @@
+#include <Union.h>
+
+int main()
+{
+    init_lib();
+    init_relay(1);
+
+    push_relay(1, 1);
+    delay(1000);
+    push_relay(1, 0);
+    delay(1000);
+    return 0;
+}
+// gcc proverka.c -o proverka -lm -lwiringPi -lptrhread -ldopLibc
